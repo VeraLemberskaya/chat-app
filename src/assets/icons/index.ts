62c103b6af90e74063f8989fda2e8451c1b7b67b
Vera Lemberskaya <veralemberskaa@gmail.com>
2023-05-01 15:25:50 +0300

    feature: add password input type;

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,2 @@
+export { ReactComponent as EyeOpenIcon } from './eye-open.svg';
+export { ReactComponent as EyeClosedIcon } from './eye-closed.svg';
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,5 @@
-import React, { ChangeEventHandler, FC } from 'react';
+import React, { ChangeEventHandler, FC, useState } from 'react';
+import { EyeClosedIcon, EyeOpenIcon } from 'assets/icons';
 
 import styles from './Input.module.scss';
 
@@ -16,12 +17,33 @@ interface IInput {
 }
 
 const Input: FC<IInput> = ({ label, name, type = 'text' }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+
+  const togglePassword = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const getInputType = () => {
+    if (type === 'password') {
+      return isPasswordVisible ? 'text' : type;
+    }
+
+    return type;
+  };
+
+  const passwordAdornment = (
+    <div className={styles.adornment} onClick={togglePassword}>
+      {isPasswordVisible ? <EyeClosedIcon /> : <EyeOpenIcon />}
+    </div>
+  );
+
   return (
     <div className={styles.inputWrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input className={styles.input} name={name} type={type} />
+      <input className={styles.input} name={name} type={getInputType()} />
+      {type === 'password' && passwordAdornment}
     </div>
   );
 };
